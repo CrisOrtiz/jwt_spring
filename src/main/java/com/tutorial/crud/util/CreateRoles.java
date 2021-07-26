a241fdcd0e00d7db20550ee75f6cc37ea15c7360
CrisOrtiz <cristhianortizmercado@gmail.com>
2021-07-26 15:16:28 -0400

    setup project

--- src/main/java/com/tutorial/crud/util/CreateRoles.java
+++ src/main/java/com/tutorial/crud/util/CreateRoles.java
@@ -1,5 +1,7 @@
 package com.tutorial.crud.util;
 
+import com.tutorial.crud.security.entity.Rol;
+import com.tutorial.crud.security.enums.RolName;
 import com.tutorial.crud.security.service.RolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,7 +21,7 @@ public class CreateRoles implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        /**Rol rolAdmin = new Rol(RolName.ROLE_ADMIN);
+         /**Rol rolAdmin = new Rol(RolName.ROLE_ADMIN);
          Rol rolUser = new Rol(RolName.ROLE_USER);
          rolService.save(rolAdmin);
          rolService.save(rolUser);*/

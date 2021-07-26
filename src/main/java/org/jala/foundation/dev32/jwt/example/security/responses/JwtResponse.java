8e104d8845b5ae6a33582889e37329e413dac12c
jorgedq <jorgedq7952@gmail.com>
2021-07-26 16:07:56 -0400

    improve login response

--- src/main/java/org/jala/foundation/dev32/jwt/example/security/responses/JwtResponse.java
+++ src/main/java/org/jala/foundation/dev32/jwt/example/security/responses/JwtResponse.java
@@ -6,14 +6,9 @@ import java.util.Collection;
 
 public class JwtResponse {
     private String token;
-    private String bearer = "Bearer";
-    private String userName;
-    private Collection<? extends GrantedAuthority> authorities;
 
-    public JwtResponse(String token, String userName, Collection<? extends GrantedAuthority> authorities) {
+    public JwtResponse(String token) {
         this.token = token;
-        this.userName = userName;
-        this.authorities = authorities;
     }
 
     public String getToken() {
@@ -22,29 +17,5 @@ public class JwtResponse {
 
     public void setToken(String token) {
         this.token = token;
-    }
-
-    public String getBearer() {
-        return bearer;
-    }
-
-    public void setBearer(String bearer) {
-        this.bearer = bearer;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        this.authorities = authorities;
     }
 }
